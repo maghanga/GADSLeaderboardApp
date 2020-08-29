8a3b0fae5969ae8e8e95adf34e1741310ead7530
maghanga <thinknehru@gmail.com>
2020-08-29 13:29:03 +0300

    [Add] splash activity delay function

--- app/src/main/java/com/example/gadsleaderboardapp/ui/SplashActivity.java
+++ app/src/main/java/com/example/gadsleaderboardapp/ui/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.gadsleaderboardapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.gadsleaderboardapp.R;
 
@@ -12,5 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LandingPageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
